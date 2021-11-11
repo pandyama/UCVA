62961801f54c6325acfcf6c48a14714f27b8169b
Meet Pandya <pandyama2@gmail.com>
2021-11-11 10:05:07 -0700

    created 4 packages for now and 2 classes in model package

---
+++ Backend/src/main/java/Model/User.java
@@ -0,0 +1,16 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    private String username;
+    private String role;
+    private String designation;
+
+}
