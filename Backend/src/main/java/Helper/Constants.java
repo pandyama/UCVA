2d635d6323c915c0d30c25dae06b0f384dced8dd
Meet Pandya <pandyama2@gmail.com>
2021-11-12 16:49:54 -0700

    Added animal model details and created 2 constants for Sex

--- Backend/src/main/java/Helper/Constants.java
+++ Backend/src/main/java/Helper/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String APPLICATION_JSON = "application/json";
     public static final String PLAIN_TXT = "text/plain";
+    public static final String MALE = "MALE";
+    public static final String FEMALE = "FEMALE";
 }
+
--- Backend/src/main/java/Model/Animal.java
+++ Backend/src/main/java/Model/Animal.java
@@ -1,4 +1,26 @@
 package Model;
 
 public class Animal {
+
+    private int tattooNumber;
+    private String cityTattoo;
+    private String name;
+    private int age;
+    private double weight;
+    private String sex;
+    private String bDay;
+    private String bMonth;
+    private String bYear;
+    private String breed;
+    private String coatColor;
+    private String problem;
+    private String comment;
+    private String[] continuousMedication;
+    private String specialInstructions;
+    private String specialDiet;
+    private int active;
+    private int RFID;
+    private int microchip;
+
+
 }
