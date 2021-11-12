8feebfa26426dbfc85c2b84cecb0d2dd99314ac5
Meet Pandya <pandyama2@gmail.com>
2021-11-12 16:20:30 -0700

    added code in userController for login endpoint and created 2 more constants in Constants file

--- Backend/src/main/java/Controller/UserController.java
+++ Backend/src/main/java/Controller/UserController.java
@@ -1,4 +1,28 @@
 package Controller;
 
+import Helper.Constants;
+import com.sun.net.httpserver.HttpServer;
+
 public class UserController {
+
+    private HttpServer server;
+    public UserController(HttpServer s1){
+        this.server = s1;
+    }
+
+    public void login(){
+        this.server.createContext("/login",(exchange -> {
+            if(checkRequest(Constants.GET,exchange.getRequestMethod())){
+                // Take user to login page
+            }
+            else if(checkRequest(Constants.POST,exchange.getRequestMethod())){
+                // Authenticate User
+            }
+        }));
+    }
+
+    public boolean checkRequest(String s1, String s2){
+        return s1.equals(s2);
+    }
+
 }
