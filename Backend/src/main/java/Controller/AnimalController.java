68cab3b756935524b9b1078a6cf33e0a81a4006c
Meet Pandya <pandyama2@gmail.com>
2021-11-12 17:21:55 -0700

    Added code for AddAnimal Post request and Get request

--- Backend/src/main/java/Controller/AnimalController.java
+++ Backend/src/main/java/Controller/AnimalController.java
@@ -1,6 +1,13 @@
 package Controller;
 import Helper.Constants;
-import org.apache.http.impl.bootstrap.HttpServer;
+import Helper.HelperMethods;
+import Helper.StatusCode;
+import com.sun.net.httpserver.HttpServer;
+import kong.unirest.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class AnimalController {
 
@@ -10,7 +17,23 @@ public class AnimalController {
     }
 
     public void addAnimal(){
-        
+        this.server.createContext("/addAnimal", (exchange -> {
+            if (HelperMethods.checkRequest(Constants.POST, exchange.getRequestMethod())) {
+                // Get Animal data from request and submit to Database
+                InputStreamReader is = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+                BufferedReader br = new BufferedReader(is);
+                JSONObject requestData = new JSONObject(br.readLine());
+                System.out.println(requestData.get("Name"));
+
+            }
+            else if(HelperMethods.checkRequest(Constants.GET, exchange.getRequestMethod())){
+                // Respond back saying request not allowed
+                exchange.sendResponseHeaders(StatusCode.METHOD_NOT_ALLOWED.getCode(),"Method not allowed".getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write("Method not allowed".getBytes());
+                output.flush();
+            }
+        }));
     }
 
 }
