3aa548a0fde624bdca105f8bef62b81172550cb9
Meet Pandya <pandyama2@gmail.com>
2021-11-12 17:00:29 -0700

    Added constructor and empty function block inside AnimalController

--- Backend/src/main/java/Controller/AnimalController.java
+++ Backend/src/main/java/Controller/AnimalController.java
@@ -1,4 +1,16 @@
 package Controller;
+import Helper.Constants;
+import org.apache.http.impl.bootstrap.HttpServer;
 
 public class AnimalController {
+
+    private HttpServer server;
+    public AnimalController(HttpServer s){
+        this.server = s;
+    }
+
+    public void addAnimal(){
+        
+    }
+
 }
